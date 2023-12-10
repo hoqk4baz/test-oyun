5a3a673e74c68f8b58593e92b35c325af3c4945e
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2023-12-10 17:07:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,125 +1,75 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+import threading
+import urllib.parse
 import requests
 import xml.etree.ElementTree as ET
 
-class RequestHandler(BaseHTTPRequestHandler):
+class Handler(BaseHTTPRequestHandler):
+    def _set_response(self, content_type='text/html'):
+        self.send_response(200)
+        self.send_header('Content-type', content_type)
+        self.end_headers()
+
     def do_GET(self):
         if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-
-            # HTML içeriği buraya ekleyin
-            html_content = """
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Game Code Redemption</title>
-            </head>
-            <body>
-                <h1>Game Code Redemption</h1>
-                
-                <form method="post" action="/redeem">
-                    <button type="submit" name="game_id" value="1">Animal Nursery</button>
-                    <button type="submit" name="game_id" value="2">Car Club</button>
-                </form>
-
-                <label for="code">Enter the numeric code:</label>
-                <input type="text" id="code" name="code" required>
-                <button type="submit" form="redeem-form">Redeem</button>
-
-                <p>{result}</p>
-            </body>
-            </html>
-            """.format(result="")
-
-            self.wfile.write(html_content.encode('utf-8'))
+            with open('index.html', 'rb') as file:
+                self._set_response()
+                self.wfile.write(file.read())
         else:
-            self.send_error(404, 'Not Found')
+            self.send_error(404)
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
-        post_params = dict(param.split('=') for param in post_data.split('&'))
+        post_data = self.rfile.read(content_length)
+        post_params = urllib.parse.parse_qs(post_data.decode('utf-8'))
 
-        if self.path == '/redeem':
-            game_id = int(post_params.get('game_id', 0))
-            code = post_params.get('code', '')
+        if self.path == '/animal_nursery':
+            kod = post_params['kod'][0]
+            sonuc = self.use_code_animal_nursery(kod)
+            self._set_response()
+            self.wfile.write(sonuc.encode('utf-8'))
 
-            if game_id == 1:
-                result = self.redeem_animal_nursery_code(code)
-            elif game_id == 2:
-                result = self.redeem_car_club_code(code)
-            else:
-                result = "Invalid selection."
+        elif self.path == '/car_club':
+            kod = post_params['kod'][0]
+            sonuc = self.use_code_car_club(kod)
+            self._set_response()
+            self.wfile.write(sonuc.encode('utf-8'))
 
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            
-            html_content = """
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Game Code Redemption</title>
-            </head>
-            <body>
-                <h1>Game Code Redemption</h1>
-                
-                <form method="post" action="/redeem" id="redeem-form">
-                    <button type="submit" name="game_id" value="1">Animal Nursery</button>
-                    <button type="submit" name="game_id" value="2">Car Club</button>
-                </form>
-
-                <label for="code">Enter the numeric code:</label>
-                <input type="text" id="code" name="code" required>
-                <button type="submit" form="redeem-form">Redeem</button>
-
-                <p>{result}</p>
-            </body>
-            </html>
-            """.format(result=result)
-
-            self.wfile.write(html_content.encode('utf-8'))
         else:
-            self.send_error(404, 'Not Found')
+            self.send_error(404)
 
-    def redeem_animal_nursery_code(self, code):
-        # Burada gerçek redemption mantığınızı ekleyin
-        xml_data = requests.get("https://vodafonecarclub.apphicgames.com/WebService1.asmx/Use_Code_AnimalNursery?code="+code+"&pin=app2022?*1", verify=False).text
-        root = ET.fromstring(xml_data)
-        result = root.text
-
-        if result == "2":
+    def use_code_animal_nursery(self, kod):
+        xml_data = requests.get("https://vodafonecarclub.apphicgames.com/WebService1.asmx/Use_Code_AnimalNursery?code="+kod+"&pin=app2022?*1", verify=False).text
+        dark = ET.fromstring(xml_data)
+        sonuc = dark.text
+        if sonuc == "2":
             return "[+] 2GB Alındı"
-        elif result == "1":
+        elif sonuc == "1":
             return "[*] Kodu Zaten Kullanmışsın"
-        elif result == "0":
+        elif sonuc == "0":
             return "[!] Yanlış Kod veya Yanlış oyun Seçtin"
         else:
             return "[x] Bir Sorun oluştu"
 
-    def redeem_car_club_code(self, code):
-        # Burada gerçek redemption mantığınızı ekleyin
-        xml_data = requests.get("https://vodafonecarclub.apphicgames.com/WebService1.asmx/Use_Code_CarRacing?code="+code+"&pin=app2022?*1", verify=False).text
-        root = ET.fromstring(xml_data)
-        result = root.text
-
-        if result == "2":
+    def use_code_car_club(self, kod):
+        xml_data = requests.get("https://vodafonecarclub.apphicgames.com/WebService1.asmx/Use_Code_CarRacing?code="+kod+"&pin=app2022?*1", verify=False).text
+        dark = ET.fromstring(xml_data)
+        sonuc = dark.text
+        if sonuc == "2":
             return "[+] 2GB Alındı"
-        elif result == "1":
+        elif sonuc == "1":
             return "[*] Kodu Zaten Kullanmışsın"
-        elif result == "0":
+        elif sonuc == "0":
             return "[!] Yanlış Kod veya Yanlış oyun Seçtin"
         else:
             return "[x] Bir Sorun oluştu"
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
 
 if __name__ == '__main__':
-    port = 8000
-    server = HTTPServer(('localhost', port), RequestHandler)
-    print(f'Server started on http://localhost:{port}')
+    server = ThreadedHTTPServer(('localhost', 8080), Handler)
+    print('Server running on port 8080...')
     server.serve_forever()
+
